e87450e65ba9747269f8ccfbea4ae8f83524615f
Janek Góral <yaneq6@gmail.com>
2019-07-11 14:15:29 +0200

    Add expandable list test code

--- example-databinding/src/main/java/com/xwray/groupie/example/databinding/MainActivity.java
+++ example-databinding/src/main/java/com/xwray/groupie/example/databinding/MainActivity.java
@@ -43,6 +43,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import jp.wasabeef.recyclerview.animators.FadeInAnimator;
+import jp.wasabeef.recyclerview.animators.SlideInDownAnimator;
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String INSET_TYPE_KEY = "inset_type";
@@ -94,14 +98,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(new InsetItemDecoration(gray, betweenPadding));
         recyclerView.addItemDecoration(new DebugItemDecoration(this));
         recyclerView.setAdapter(groupAdapter);
-        recyclerView.addOnScrollListener(new InfiniteScrollListener(layoutManager) {
-            @Override public void onLoadMore(int currentPage) {
-                int color = rainbow200[currentPage % rainbow200.length];
-                for (int i = 0; i < 5; i++) {
-                    infiniteLoadingSection.add(new CardItem(color));
-                }
-            }
-        });
+        recyclerView.setItemAnimator(new FadeInAnimator());
+//        recyclerView.addOnScrollListener(new InfiniteScrollListener(layoutManager) {
+//            @Override public void onLoadMore(int currentPage) {
+//                int color = rainbow200[currentPage % rainbow200.length];
+//                for (int i = 0; i < 5; i++) {
+//                    infiniteLoadingSection.add(new CardItem(color));
+//                }
+//            }
+//        });
 
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(touchCallback);
         itemTouchHelper.attachToRecyclerView(recyclerView);
@@ -117,88 +122,91 @@ public class MainActivity extends AppCompatActivity {
 
     private void populateAdapter() {
 
-        // Full bleed item
-        Section fullBleedItemSection = new Section(new HeaderItem(R.string.full_bleed_item));
-        fullBleedItemSection.add(new FullBleedCardItem(R.color.purple_200));
-        groupAdapter.add(fullBleedItemSection);
-
-        // Update in place group
-        Section updatingSection = new Section();
-        View.OnClickListener onShuffleClicked = new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                List<UpdatableItem> shuffled = new ArrayList<>(updatableItems);
-                Collections.shuffle(shuffled);
-                updatingGroup.update(shuffled);
-
-                // You can also do this by forcing a change with payload
-                binding.recyclerView.post(new Runnable() {
-                    @Override public void run() {
-                        binding.recyclerView.invalidateItemDecorations();
-                    }
-                });
-            }
-        };
-        HeaderItem updatingHeader = new HeaderItem(
-                R.string.updating_group,
-                R.string.updating_group_subtitle,
-                R.drawable.shuffle,
-                onShuffleClicked);
-        updatingSection.setHeader(updatingHeader);
-        updatingGroup = new Section();
-        updatableItems = new ArrayList<>();
-        for (int i = 1; i <= 12; i++) {
-            updatableItems.add(new UpdatableItem(rainbow200[i], i));
-        }
-        updatingGroup.update(updatableItems);
-        updatingSection.add(updatingGroup);
-        groupAdapter.add(updatingSection);
+//        // Full bleed item
+//        Section fullBleedItemSection = new Section(new HeaderItem(R.string.full_bleed_item));
+//        fullBleedItemSection.add(new FullBleedCardItem(R.color.purple_200));
+//        groupAdapter.add(fullBleedItemSection);
+//
+//        // Update in place group
+//        Section updatingSection = new Section();
+//        View.OnClickListener onShuffleClicked = new View.OnClickListener() {
+//            @Override public void onClick(View view) {
+//                List<UpdatableItem> shuffled = new ArrayList<>(updatableItems);
+//                Collections.shuffle(shuffled);
+//                updatingGroup.update(shuffled);
+//
+//                // You can also do this by forcing a change with payload
+//                binding.recyclerView.post(new Runnable() {
+//                    @Override public void run() {
+//                        binding.recyclerView.invalidateItemDecorations();
+//                    }
+//                });
+//            }
+//        };
+//        HeaderItem updatingHeader = new HeaderItem(
+//                R.string.updating_group,
+//                R.string.updating_group_subtitle,
+//                R.drawable.shuffle,
+//                onShuffleClicked);
+//        updatingSection.setHeader(updatingHeader);
+//        updatingGroup = new Section();
+//        updatableItems = new ArrayList<>();
+//        for (int i = 1; i <= 12; i++) {
+//            updatableItems.add(new UpdatableItem(rainbow200[i], i));
+//        }
+//        updatingGroup.update(updatableItems);
+//        updatingSection.add(updatingGroup);
+//        groupAdapter.add(updatingSection);
 
         // Expandable group
-        ExpandableHeaderItem expandableHeaderItem = new ExpandableHeaderItem(R.string.expanding_group, R.string.expanding_group_subtitle);
-        ExpandableGroup expandableGroup = new ExpandableGroup(expandableHeaderItem);
-        for (int i = 0; i < 2; i++) {
-            expandableGroup.add(new CardItem(rainbow200[1]));
+
+
+        for (int i = 0; i < 5; i++) {
+            ExpandableHeaderItem expandableHeaderItem = new ExpandableHeaderItem(R.string.expanding_group, R.string.expanding_group_subtitle);
+            ExpandableGroup expandableGroup = new ExpandableGroup(expandableHeaderItem);
+            for (int j = 0; j < 5; j++) {
+                expandableGroup.add(new CardItem(rainbow200[1]));
+            }
+            groupAdapter.add(expandableGroup);
         }
-        groupAdapter.add(expandableGroup);
-
-        // Columns
-        Section columnSection = new Section(new HeaderItem(R.string.vertical_columns));
-        ColumnGroup columnGroup = makeColumnGroup();
-        columnSection.add(columnGroup);
-        groupAdapter.add(columnSection);
-
-        // Group showing even spacing with multiple columns
-        Section multipleColumnsSection = new Section(new HeaderItem(R.string.multiple_columns));
-        for (int i = 0; i < 12; i++) {
-            multipleColumnsSection.add(new SmallCardItem(rainbow200[5]));
-        }
-        groupAdapter.add(multipleColumnsSection);
-
-        // Swipe to delete (with add button in header)
-        swipeSection = new Section(new HeaderItem(R.string.swipe_to_delete));
-        for (int i = 0; i < 3; i++) {
-            swipeSection.add(new SwipeToDeleteItem(rainbow200[6]));
-        }
-        groupAdapter.add(swipeSection);
-
-        // Horizontal carousel
-        Section carouselSection = new Section(new HeaderItem(R.string.carousel, R.string.carousel_subtitle));
-        carouselSection.setHideWhenEmpty(true);
-        Group carousel = makeCarouselGroup();
-        carouselSection.add(carousel);
-        groupAdapter.add(carouselSection);
-
-        // Update with payload
-        Section updateWithPayloadSection = new Section(new HeaderItem(R.string.update_with_payload, R.string.update_with_payload_subtitle));
-        for (int i = 0; i < rainbow500.length; i++) {
-            updateWithPayloadSection.add(new HeartCardItem(rainbow200[i], i, onFavoriteListener));
-
-        }
-        groupAdapter.add(updateWithPayloadSection);
-
-        // Infinite loading section
-        infiniteLoadingSection = new Section(new HeaderItem(R.string.infinite_loading));
-        groupAdapter.add(infiniteLoadingSection);
+//        // Columns
+//        Section columnSection = new Section(new HeaderItem(R.string.vertical_columns));
+//        ColumnGroup columnGroup = makeColumnGroup();
+//        columnSection.add(columnGroup);
+//        groupAdapter.add(columnSection);
+//
+//        // Group showing even spacing with multiple columns
+//        Section multipleColumnsSection = new Section(new HeaderItem(R.string.multiple_columns));
+//        for (int i = 0; i < 12; i++) {
+//            multipleColumnsSection.add(new SmallCardItem(rainbow200[5]));
+//        }
+//        groupAdapter.add(multipleColumnsSection);
+//
+//        // Swipe to delete (with add button in header)
+//        swipeSection = new Section(new HeaderItem(R.string.swipe_to_delete));
+//        for (int i = 0; i < 3; i++) {
+//            swipeSection.add(new SwipeToDeleteItem(rainbow200[6]));
+//        }
+//        groupAdapter.add(swipeSection);
+//
+//        // Horizontal carousel
+//        Section carouselSection = new Section(new HeaderItem(R.string.carousel, R.string.carousel_subtitle));
+//        carouselSection.setHideWhenEmpty(true);
+//        Group carousel = makeCarouselGroup();
+//        carouselSection.add(carousel);
+//        groupAdapter.add(carouselSection);
+//
+//        // Update with payload
+//        Section updateWithPayloadSection = new Section(new HeaderItem(R.string.update_with_payload, R.string.update_with_payload_subtitle));
+//        for (int i = 0; i < rainbow500.length; i++) {
+//            updateWithPayloadSection.add(new HeartCardItem(rainbow200[i], i, onFavoriteListener));
+//
+//        }
+//        groupAdapter.add(updateWithPayloadSection);
+//
+//        // Infinite loading section
+//        infiniteLoadingSection = new Section(new HeaderItem(R.string.infinite_loading));
+//        groupAdapter.add(infiniteLoadingSection);
     }
 
     private ColumnGroup makeColumnGroup() {
